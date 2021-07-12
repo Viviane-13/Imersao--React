645275558ed54072a2a8e581bdf237b9f26dcbbe
Viviane <oviviane772@gmail.com>
2021-07-12 12:10:23 -0300

    reset

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+  /* Reset CSS */
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: sans-serif;
+    background-color: #D9E6F6;
   }
 `
 
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+const Box = styled.div`
+  background: #FFFFFF;
+  border-radius: 8px;
+`;
 
 export default function Home() {
   return (
     <main>
-      kkkkkkk
+      <Box>
+        Imagem
+      </Box>
+      <Box>
+        Bem vindo
+      </Box>
+      <Box>
+        Comunidades
+      </Box>
     </main>
   )
 }
